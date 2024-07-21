7ea3574e6f3e16f8c3355eef221e2dc4a22138e2
Jessica Montserrat Patiño Flores <jessi_flo@live.com.mx>
2024-07-20 19:50:23 -0600

    first commit jessi

--- prueba.js
+++ prueba.js
@@ -1,20 +1,3 @@
-
-// Prueba
-console.log("hola mundo");
-
-console.log("hola mundo 2");
-
-console.log("hola mundo 3")
-
-// Prueba
-
-
-console.log("hola mundo 2")
-
-console.log("hola mundo");
-
-
-for(;;){
-    console.log(" :P ");
-}
-
+const hola = () => {
+  console.log("Hola soy Jessi desde Jessi");
+};
