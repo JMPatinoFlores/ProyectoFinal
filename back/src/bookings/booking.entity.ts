1b2c9fb744630298d297c7d51ac7efb9bad95f2b
BrunoChampionGalvez <bruno.champion.g@upch.pe>
2024-07-23 21:38:42 -0500

    Scaffolding de las entidades Bookings y BookingDetails.

---
+++ back/src/bookings/booking.entity.ts
@@ -0,0 +1,18 @@
+import { BookingDetails } from "src/bookingDetails/booking-detail.entity";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'bookings'})
+export class Booking {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column()
+    date: string
+
+    @Column()
+    time: string
+
+    @OneToOne(() => BookingDetails)
+    @JoinColumn({name: "booking-details-id"})
+    bookingDetails: BookingDetails
+}
