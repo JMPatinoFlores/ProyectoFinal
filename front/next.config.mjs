6c63a7ec02bf1cc46b1b1f0c24576dfdb44d5500
Jessica Montserrat Patiño Flores <jessi_flo@live.com.mx>
2024-07-27 16:30:59 -0600

    registro de usuario, hotelero, registro de hotel con faltantes, registro de imagen de hotel

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -4,11 +4,32 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     dotenv: {
-      path: './.env', // Load environment variables from .env file
+      path: "./.env", // Load environment variables from .env file
     },
   },
   eslint: {
     // Enable ESLint support for Babel plugins/presets
     ignoreDuringBuilds: true,
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
+          },
+        ],
+      },
+    ];
   },
 };
