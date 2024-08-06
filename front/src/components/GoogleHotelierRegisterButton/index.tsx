f7e1d773e8c2406dcb80b778afa44ed74a22f0bb
Jessica Montserrat Patiño Flores <jessi_flo@live.com.mx>
2024-08-05 22:44:14 -0600

    Rutas de front actualizadas para registro y login

--- front/src/components/GoogleHotelierRegisterButton/index.tsx
+++ front/src/components/GoogleHotelierRegisterButton/index.tsx
@@ -10,7 +10,7 @@ const GoogleHotelierRegisterButton = () => {
 
   const handleGoogleRegister = () => {
     window.location.href =
-      "http://localhost:3000/api/auth/google/register/hotelAdmin";
+      "http://localhost:3000/auth/api/google/register/hotelAdmin";
   };
 
   useEffect(() => {
