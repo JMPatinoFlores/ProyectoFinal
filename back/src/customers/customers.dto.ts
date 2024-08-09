2d4722f5eadf504ce625d61a2530f626af633b31
HernanD <projectmgrex@hotmail.com>
2024-08-09 18:58:19 -0500

    Notificaciones por Registro en usuario

--- back/src/customers/customers.dto.ts
+++ back/src/customers/customers.dto.ts
@@ -11,24 +11,40 @@ import {
 } from 'class-validator';
 
 export class CreateCustomerDto {
+  /**
+   * Debe tener entre 3 y 50 caracteres
+   * @example 'Test'
+   */
   @IsNotEmpty()
   @IsString()
   @MinLength(3)
   @MaxLength(50)
   name: string;
 
+  /**
+   * Debe tener entre 3 y 50 caracteres
+   * @example 'Customer'
+   */
   @IsNotEmpty()
   @IsString()
   @MinLength(3)
   @MaxLength(50)
   lastName: string;
 
+  /**
+   * Debe tener entre 6 y 50 caracteres, contener una mayúscula
+   * @example 'customer@mail.com'
+   */
   @IsNotEmpty()
   @IsEmail()
   @MinLength(6)
   @MaxLength(50)
   email: string;
 
+  /**
+   * Debe tener entre 8 y 12 caracteres, contener una mayúscula, número y carácter especial
+   * @example 'Test*1234'
+   */
   @IsNotEmpty()
   @IsString()
   @MinLength(6)
@@ -36,25 +52,45 @@ export class CreateCustomerDto {
   @IsStrongPassword()
   password: string;
 
+  /**
+   * Debe ser string
+   * @example '+1 12345678901'
+   */
   @IsNotEmpty()
   @IsString()
   phone: string;
 
+  /**
+   * Debe tener máx. 50 caracteres
+   * @example 'Customer Country'
+   */
   @IsNotEmpty()
   @IsString()
   @MaxLength(50)
   country: string;
 
+  /**
+   * Debe tener máx. 50 caracteres
+   * @example 'Customer City'
+   */
   @IsNotEmpty()
   @IsString()
   @MaxLength(50)
   city: string;
 
+  /**
+   * Debe tener máx. 50 caracteres
+   * @example 'Customer Address 1234'
+   */
   @IsNotEmpty()
   @IsString()
   @MaxLength(100)
   address: string;
 
+  /**
+   * Debe tener máx. 10 caracteres
+   * @example '01/01/2000'
+   */
   @IsNotEmpty()
   @IsString()
   @MaxLength(10)
--- back/src/email-notify/mail.service.ts
+++ back/src/email-notify/mail.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 import { config as config } from 'dotenv';
+import { CreateCustomerDto } from 'src/customers/customers.dto';
+import * as path from 'path';
+import * as fs from 'fs';
 
 config({ path: './.development.env' });
 @Injectable()
@@ -34,5 +37,22 @@ export class MailService {
       console.error('Error al enviar el correo: %s', error);
       throw error;
     }
+  }
+
+  async sendWelcomeEmail(customer: CreateCustomerDto) {
+    const emailTemplatePath = path.join(
+      __dirname,
+      '../helper/correos-nodemailer/correos-nodemailer/registerUser/index.html',
+    );
+
+    let htmlTemplate = fs.readFileSync(emailTemplatePath, 'utf-8');
+    htmlTemplate = htmlTemplate.replace('[Nombre del Usuario]', customer.name);
+
+    await this.sendMail(
+      customer.email,
+      'Bienvenido a Ruta Viajera',
+      'Gracias por registarte en nuestra plataforma',
+      htmlTemplate,
+    );
   }
 }
